7943e2d733ee6da493ed8312e4f170f8bb6ea0c0
mst101097 <singh.1097.mohit@gmail.com>
2024-01-21 17:16:16 +0530

    Adding new functions

--- S03_Advance_IAM_with_Python/Conditinals.py
+++ S03_Advance_IAM_with_Python/Conditinals.py
@@ -41,4 +41,4 @@ print(response)
 '''
 {'Policy': {'PolicyName': 'AccessWindowPolicyNew', 'PolicyId': 'ANPAZLIUYTA5QQ7W7NWY4', 'Arn': 'arn:aws:iam::642678036539:policy/AccessWindowPolicyNew', 'Path': '/', 'DefaultVersionId': 'v1', 'AttachmentCount': 0, 'PermissionsBoundaryUsageCount': 0, 'IsAttachable': True, 'CreateDate': datetime.datetime(2024, 1, 8, 13, 14, 15, tzinfo=tzutc()), 'UpdateDate': datetime.datetime(2024, 1, 8, 13, 14, 15, tzinfo=tzutc())}, 'ResponseMetadata': {'RequestId': '9c91125e-3573-438d-b809-265f5f4b63ff', 'HTTPStatusCode': 200, 'HTTPHeaders': {'x-amzn-requestid': '9c91125e-3573-438d-b809-265f5f4b63ff', 'content-type': 'text/xml', 'content-length': '777', 'date': 'Mon, 08 Jan 2024 13:14:15 GMT'}, 'RetryAttempts': 0}}
 
-'''
+'''w
---
+++ S04_AWS_Dynamodb/PartiQL_Query.sql
@@ -0,0 +1,16 @@
+Select * from Article;
+
+Select * from Article where id in(1,2);
+
+--insert into Article value{
+--	'id':5,
+--    'pub_date':'2022-02-12',
+--    'title':'my newest title 2',
+--    'body':'this is newest partiql body',
+--    'author':'testuser'
+--    };
+--
+DELETE
+FROM Article
+WHERE
+  id = 5 AND pub_date = '2022-02-12';
