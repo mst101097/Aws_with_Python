2c88b73e54a272f20ad33bb494e296f13f8da0c2
mst101097 <singh.1097.mohit@gmail.com>
2024-02-23 12:01:55 +0530

    Added S3 bucket code snippt

---
+++ S05_AWS_Simple_Storage_Service_S3/CreateBucketthroughPython.py
@@ -0,0 +1,18 @@
+import boto3
+
+bucket = boto3.resource('s3',region_name='us-east-1')
+
+response = bucket.create_bucket(
+    Bucket ="botolession2",
+    ACL = "private",
+
+    # if you are using default region name us-east-1 then you do not need add this constraints in parameter
+    # CreateBucketConfiguration={
+    #     'LocationConstraint':'us-east-1'
+        
+    # }
+)
+
+print(response)
+
+# s3.Bucket(name='botolession2')
